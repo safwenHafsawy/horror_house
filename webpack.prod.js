17b3b8927535976ceba44ba5c2385f4b09301f7d
safwen <safwenHafsawy>
2022-12-01 10:47:07 +0100

    fix : imported miniCssExtractPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(commonConfig, {
   mode: "production",
